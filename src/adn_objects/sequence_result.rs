fb942a9731588f4c1dad8a5ec47c3dec6974b253
Barioth <bernier.teo@gmail.com>
2021-01-30 01:56:42 +0100

    new print format

--- src/adn_objects/sequence_result.rs
+++ src/adn_objects/sequence_result.rs
@@ -138,56 +138,130 @@ impl SequenceResult {
                     );
                     println!("{}", sequence.0);
                     let result = results.0.get(&tmp.0.to_string()).unwrap();
-                    file.write_all(
-                        (format!(
-                            "{} :\n{}\n\n{}\n{}\n{}\n\n",
-                            tmp.0,
-                            sequence.1.to_string(),
-                            Sequence::get_best_alignment(
-                                genes_v.get_gene(&tmp.1 .0).unwrap(),
-                                &sequence.1
-                            )
-                            .1,
-                            Sequence::get_best_alignment(
-                                genes_d.get_gene(&tmp.1 .1).unwrap(),
-                                &sequence.1
-                            )
-                            .1,
-                            Sequence::get_best_alignment(
-                                genes_j.get_gene(&tmp.1 .2).unwrap(),
-                                &sequence.1
-                            )
-                            .1,
-                        ) + &format!(
-                            "{}\n{}\n{}\n\n",
-                            if let Some(gene) = genes_v.get_gene(&result.0) {
-                                Sequence::get_best_alignment(gene, &sequence.1).1
-                            } else {
-                                ('.'..='.')
-                                    .cycle()
-                                    .take(sequence.1.len())
-                                    .collect::<String>()
-                            },
-                            if let Some(gene) = genes_d.get_gene(&result.1) {
-                                Sequence::get_best_alignment(gene, &sequence.1).1
-                            } else {
-                                ('.'..='.')
-                                    .cycle()
-                                    .take(sequence.1.len())
-                                    .collect::<String>()
-                            },
-                            if let Some(gene) = genes_j.get_gene(&result.2) {
-                                Sequence::get_best_alignment(gene, &sequence.1).1
-                            } else {
-                                ('.'..='.')
-                                    .cycle()
-                                    .take(sequence.1.len())
-                                    .collect::<String>()
-                            },
-                        ))
-                            .as_bytes(),
+
+                    let seq = sequence.1.to_string();
+                    let seq_bytes = seq.as_bytes();
+
+                    let v_pred = Sequence::get_best_alignment(
+                        genes_v.get_gene(&tmp.1 .0).unwrap(),
+                        &sequence.1,
                     )
-                    .expect("Cannot write to file");
+                    .1;
+
+                    let d_pred = Sequence::get_best_alignment(
+                        genes_d.get_gene(&tmp.1 .1).unwrap(),
+                        &sequence.1,
+                    )
+                    .1;
+
+                    let j_pred = Sequence::get_best_alignment(
+                        genes_j.get_gene(&tmp.1 .2).unwrap(),
+                        &sequence.1,
+                    )
+                    .1;
+
+                    let v_res = if let Some(gene) = genes_v.get_gene(&result.0) {
+                        Sequence::get_best_alignment(gene, &sequence.1).1
+                    } else {
+                        ('-'..='-')
+                            .cycle()
+                            .take(sequence.1.len())
+                            .collect::<String>()
+                    };
+
+                    let d_res = if let Some(gene) = genes_d.get_gene(&result.1) {
+                        Sequence::get_best_alignment(gene, &sequence.1).1
+                    } else {
+                        ('-'..='-')
+                            .cycle()
+                            .take(sequence.1.len())
+                            .collect::<String>()
+                    };
+
+                    let j_res = if let Some(gene) = genes_j.get_gene(&result.2) {
+                        Sequence::get_best_alignment(gene, &sequence.1).1
+                    } else {
+                        ('-'..='-')
+                            .cycle()
+                            .take(sequence.1.len())
+                            .collect::<String>()
+                    };
+
+                    let preds: [&str; 3] = [&v_pred, &d_pred, &j_pred];
+                    let res: [&str; 3] = [&v_res, &d_res, &j_res];
+
+                    let check = |i: usize, seqs: [&str; 3]| {
+                        Ok(true)
+                            == seqs
+                                .iter()
+                                .map(|seq| seq.as_bytes())
+                                .try_fold(false, |b, s| match s[i] {
+                                    b'.' => Ok(b),
+                                    b'-' => Ok(true),
+                                    c if c == seq_bytes[i] => {
+                                        if !b {
+                                            Ok(true)
+                                        } else {
+                                            Err(())
+                                        }
+                                    }
+                                    _ => Err(()),
+                                })
+                    };
+
+                    let indices: Vec<_> = (0..seq_bytes.len())
+                        .filter(|&i| !check(i, preds) || !check(i, res))
+                        .collect();
+
+                    let seq_to_string = |idx: &[usize], seq: &str| -> String {
+                        let mut s = if idx[0] > 6 {
+                            format!("[{}]-{}", idx[0], &seq[idx[0]..=idx[0]])
+                        } else {
+                            (&seq[0..=idx[0]]).to_string()
+                        };
+
+                        let mut last_i = idx[0];
+
+                        for &i in idx.iter().skip(1) {
+                            if i > last_i + 7 {
+                                s += &format!("-[{}]-{}", i - last_i - 1, &seq[i..=i]);
+                            } else {
+                                s += &seq[last_i + 1..=i];
+                            }
+                            last_i = i;
+                        }
+
+                        if seq.len() - 1 > last_i + 6 {
+                            s += &format!("-[{}]\n", seq.len() - last_i - 2);
+                        } else {
+                            s = s + &seq[last_i + 1..] + "\n";
+                        };
+                        s
+                    };
+
+                    if indices.is_empty() {
+                        file.write_all(
+                            format!("Sequence : {}\n--Matches Perfectly--\n\n", tmp.0).as_bytes(),
+                        )
+                        .expect("Cannot write to file");
+                    } else {
+                        file.write_all(
+                            format!(
+                                "Sequence : {}\n{}\nPredictions :\n{}\n\nResults :\n{}\n\n\n",
+                                tmp.0,
+                                seq_to_string(&indices, &seq),
+                                preds
+                                    .iter()
+                                    .map(|seq| seq_to_string(&indices, seq))
+                                    .collect::<String>(),
+                                res.iter()
+                                    .map(|seq| seq_to_string(&indices, seq))
+                                    .collect::<String>(),
+                            )
+                            .as_bytes(),
+                        )
+                        .expect("Cannot write to file");
+                    }
                     tmp
                 })
                 .collect(),
